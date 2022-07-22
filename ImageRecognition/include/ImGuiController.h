77c73faea2a7d81df8d5a367b965d80910d8b081
aghio-2020 <ghioalejandroes@gmail.com>
2022-07-22 12:55:17 -0300

    include imgui in implementation

--- ImageRecognition/include/ImGuiController.h
+++ ImageRecognition/include/ImGuiController.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_opengl3.h"
-#include "imgui/imgui_impl_glfw.h"
-
 class ImGuiController
 {
 public:
--- ImageRecognition/src/ImGuiController.cpp
+++ ImageRecognition/src/ImGuiController.cpp
@@ -1,3 +1,7 @@
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/imgui_impl_glfw.h>
+
 #include "ImGuiController.h"
 
 // call the bindings and set preferences for the window style
