52a681ca3cb7587d5b193eece2cf43810d429198
aghio-2020 <ghioalejandroes@gmail.com>
2022-08-02 17:41:35 -0300

    remove cv unused class

--- ImageRecognition/app.cpp
+++ ImageRecognition/app.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <cstdlib>
 
+
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
